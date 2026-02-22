0205ed26296c9d467d50d21372df462553a7b183
Seidy <seidyalanis@gmail.com>
2026-02-21 19:48:47 -0600

    Cambios.

--- controladores/vehiculo.js
+++ controladores/vehiculo.js
@@ -25,12 +25,12 @@ const editarVehiculo = async (req, res) => {
     try {
         const id = req.params.id;
         const body = req.body;
-        const opciopnes = { new: true };
-        const updatedVehiculo = await Vehiculo.findByIdAndUpdate(id, body, opciopnes);
+        const opciones = { new: true };
+        const updatedVehiculo = await Vehiculo.findByIdAndUpdate(id, body, opciones);
         if (!updatedVehiculo) { 
             return res.status(404);
         }
-        res.json(updatedVehiculo);
+       res.status(200).json(updatedVehiculo);
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
@@ -53,4 +53,4 @@ module.exports = {
     crearVehiculo,
     editarVehiculo,
     eliminarVehiculo
-};
+}; 
