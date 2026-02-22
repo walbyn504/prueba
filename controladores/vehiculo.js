16996398853e1ccac421e6ef33707d94723f0c8b
Seidy <seidyalanis@gmail.com>
2026-02-21 22:03:13 -0600

    Modificaciones finales.

--- controladores/vehiculo.js
+++ controladores/vehiculo.js
@@ -43,14 +43,38 @@ const eliminarVehiculo = async (req, res) => {
         if (!deletedVehiculo) {
             return res.status(404);
         }
-        res.status(200);
+        res.status(200).json(deletedVehiculo);
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
-};4
+};
+
+
+const marcarVendido = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        // Actualiza solo el estado a "vendido"
+        const vehiculoActualizado = await Vehiculo.findByIdAndUpdate(
+            id,
+            { estado: 'vendido' },
+            { new: true } // devuelve el documento actualizado
+        );
+
+        if (!vehiculoActualizado) {
+           return res.status(404);
+        }
+
+        res.status(200).json(vehiculoActualizado);
+
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
 
 module.exports = {
     crearVehiculo,
     editarVehiculo,
-    eliminarVehiculo
+    eliminarVehiculo,
+    marcarVendido
 }; 
--- rutas/vehiculos.js
+++ rutas/vehiculos.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const {
     crearVehiculo,
     editarVehiculo,
-    eliminarVehiculo
+    eliminarVehiculo,
+    marcarVendido
 } = require('../controladores/vehiculo');
 
 //Post Method
@@ -16,5 +17,7 @@ router.put('/vehiculo/:id', editarVehiculo);
 // Eliminar vehículo por ID
 router.delete('/vehiculo/:id', eliminarVehiculo);
 
+// Cambiar a vendido
+router.patch('/vehiculo/vendido/:id', marcarVendido);
 
 module.exports = router;
